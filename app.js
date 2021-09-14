cde9d7c1a3a9f649c40b79d58aacd44c00e642e6
Jair <jairaceves56@gmail.com>
2021-09-13 20:12:10 -0500

    Creamos el archivo tabla-5.txt

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-
+const fs = require('fs');
 
 // 
 
 
-const base = 5;
+const base = 3;
 
 console.clear();
 console.log('====================');
@@ -11,8 +11,20 @@ console.log('   Tabla del: '+ base);
 console.log('====================');
 
 
+let salida = '';
+
 for(let i = 1; i <= 10; i++){
-    console.log(`${base} x ${ i } = ${ base * i }`);
+    salida += (`${base} x ${ i } = ${ base * i }\n`);
 }
-console.log('====================');
+
+console.log(salida);
+
+
+fs.writeFile( `tabla-${base}.txt`, salida, (err) => {
+    if(err) throw err;
+
+    console.log(`Tabla-${base}.txt Creado`);
+})
+
+
 
