603173a605a91852ccf927bc68e76e5307eb48a9
Jair <jairaceves56@gmail.com>
2021-09-15 20:02:26 -0500

    Inicio con Java

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
-const fs = require('fs');
+const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
 
-// 
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
-
-const base = 3;
-
-console.clear();
-console.log('====================');
-console.log('   Tabla del: '+ base);
-console.log('====================');
+let comando = argv._[0];
 
 
-let salida = '';
+switch (comando) {
 
-for(let i = 1; i <= 10; i++){
-    salida += (`${base} x ${ i } = ${ base * i }\n`);
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: `, colors.green(archivo)))
+            .catch(e => console.log(e));
+        break;
+
+    default:
+        console.log('Comando no reconocido');
+
 }
 
-console.log(salida);
 
 
-fs.writeFile( `tabla-${base}.txt`, salida, (err) => {
-    if(err) throw err;
-
-    console.log(`Tabla-${base}.txt Creado`);
-})
+// console.log(argv.base);
 
 
 
+// let parametro = argv[2];
+// let base = parametro.split('=')[1]
